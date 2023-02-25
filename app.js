9b45e230aae44aecf99546bdc13ca38517073ecf
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-25 15:15:11 -0300

    Correção
    buscaEndereco()

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const logradouro = document.querySelector("#logradouro");
 const bairro = document.querySelector("#bairro");
 const localidade = document.querySelector("#localidade");
 const uf = document.querySelector("#uf");
-const btnBuscar =  document.querySelector("#botao_buscar");
-const btnLimpar =  document.querySelector("#botao_limpar");
-const btnSalvar =  document.querySelector("#botao_salvar"); 
-let endereco; 
+const btnBuscar = document.querySelector("#botao_buscar");
+const btnLimpar = document.querySelector("#botao_limpar");
+const btnSalvar = document.querySelector("#botao_salvar");
+let enderecoResponse;
 
 btnBuscar.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    try{
-        //validaCEP();
+    e.preventDefault();
+    try {
+        validaCEP();
     }catch(erro){
-        //mensagemErro.innerHTML = erro.message;
+        mensagemErro.innerHTML = erro.message;
     }
 })
 
@@ -27,7 +27,36 @@ btnSalvar.addEventListener('click', () => {
     limparCampos();
 })
 
-function limpaCampos(){
+function buscaEndereco() {
+    fetch(`http://viacep.com.br/ws/${cepbusca.value}/json`)
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((endereco) => {
+            enderecoResponse = endereco;
+            preencheCampos(endereco);
+        });
+}
+
+function preencheCampos(endereco) {
+    for (const campo in endereco) {
+        if (document.querySelector("#" + campo)) {
+            document.querySelector("#" + campo).value = endereco[campo];
+        }
+    }
+}
+
+function validaCEP() {
+    const regex = /^\d{8}$/;
+
+    if (regex.test(cepbusca.value)) {
+        buscaEndereco();
+    } else {
+        throw new Error("CEP inválido!\nObs: Use apenas números.");
+    };
+}
+
+function limparCampos() {
     cepbusca.value = "";
     logradouro.value = "";
     bairro.value = "";
@@ -35,6 +64,7 @@ function limpaCampos(){
     uf.value = "";
     mensagemErro.innerHTML = "";
 }
+
 
 
 
